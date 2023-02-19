b0dc80aa8573789c32b352bd63c5e68015128553
dvlunev <dvlunev@ya.ru>
2023-02-19 11:17:06 +0300

    первый коммит

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+import train.dao.BookDao;
+import train.dao.impl.BookDaoImpl;
+import train.model.Author;
+import train.model.Book;
+
+import java.sql.*;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException {
+
+
+    }
+}
